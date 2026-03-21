f96931b517602d36c162d5ddc7ac407568df75bc
hayao <shun819.mail@gmail.com>
2026-03-21 18:02:23 +0900

    feat: Add hayao0819 SSH public keys to more-jump-more

--- nixos/more-jump-more/default.nix
+++ nixos/more-jump-more/default.nix
@@ -130,8 +130,18 @@
     initialPassword = "password";
     shell = pkgs.fish;
     openssh.authorizedKeys.keys = [
-      # TODO: Add your SSH public key
-      # "ssh-ed25519 AAAA... user@host"
+      # hayao0819 - https://github.com/hayao0819.keys
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEz3ezIgiwbphtKP4zvHtAwyqUL+V+cz2k9DE9lsMA2/"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBsUNCEGNnXLxDlutnbifeorEfa9ESJKvyupLc+nigaX"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOEpke7Hffr3izxbvSR8h0YBVspo9GW/z0o/Nh2gkgmj"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ6XE1iBNLdoR2/fQHkTzU/UCwmkRc16mBDyzrJd8LrS"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDJltK3IAPFaIdr0t7+GDbOQU5HJYYxoe187tD02TofA"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMNdlRMbYOKk+IK0fwRJAG1UPbSipgBMX5w6+J8LzA7x"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFUSSN6zWJV9JXkGDC1tHJWkR7KfmqK5WLDJe2vz/LTI"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDhlsi4uUAHlln9fQGosDpHES2ioI/AAwPBkq0cU3k1B"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBy30rfndPXU2tZ6k1CzdJkt9un3Loa7TWdmi3oZQVZ0"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICVTZmV36YsXTlGVKKegRaG/TOz9MACtcvZPDB2AdJUt"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOcWDTCBypBH1Z8XGIcBW7F8Po7nQqMThMM87FGnVo7V"
     ];
   };
 
